d91f1c9edc41c7d9b52388c9259a08b6ed15e16e
Lance Warshawsky <lance.warshawsky@gmail.com>
2023-05-13 22:32:37 +0300

    storage stuff

--- main.js
+++ main.js
@@ -1,59 +1,33 @@
-window.onload = function() {
-    const request = indexedDB.open("cat-facts-db", 1);
+const getFact = () => {
+    fetch("https://cat-fact.herokuapp.com/facts")
+    .then(response => response.json())
+    .then(facts => {
+        const randomIndex = Math.floor(Math.random() * facts.length);
+        const fact = facts[randomIndex];
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = fact.text;
+        const factElement = document.getElementById("facts");
+        factElement.innerHTML = "";
+        factElement.appendChild(nameElement);
 
-    request.onerror = function(event) {
-        console.error("Error opening database:", event.target.errorCode);
-    };
+      
+        localStorage.setItem("catFact", fact.text);
 
-    request.onupgradeneeded = function(event) {
-        const db = event.target.result;
+        fetch("https://cataas.com/cat")
+        .then(response => {
+            const catImage = document.createElement("img");
+            catImage.src = response.url;
+            catImage.alt = "Random cat image";
+            catImage.width = 300; 
+            factElement.appendChild(catImage);
 
-        const catFactsStore = db.createObjectStore("cat-facts", { keyPath: "id" });
-
-        const catImagesStore = db.createObjectStore("cat-images", { autoIncrement: true });
-    };
-
-    request.onsuccess = function(event) {
-        const db = event.target.result;
-
-        const getFact = () => {
-            fetch("https://cat-fact.herokuapp.com/facts")
-            .then(response => response.json())
-            .then(facts => {
-                const randomIndex = Math.floor(Math.random() * facts.length);
-                const fact = facts[randomIndex];
-
-                const transaction = db.transaction("cat-facts", "readwrite");
-                const store = transaction.objectStore("cat-facts");
-                store.add(fact);
-
-                const nameElement = document.createElement("h2");
-                nameElement.textContent = fact.text;
-                const factElement = document.getElementById("facts");
-                factElement.innerHTML = "";
-                factElement.appendChild(nameElement);
-
-                fetch("https://cataas.com/cat")
-                .then(response => {
-                    const catImage = document.createElement("img");
-                    catImage.src = response.url;
-                    catImage.alt = "Random cat image";
-                    catImage.width = 300; 
-                    factElement.appendChild(catImage);
-
-                    const transaction = db.transaction("cat-images", "readwrite");
-                    const store = transaction.objectStore("cat-images");
-                    store.add(response.blob());
-                })
-                .catch(err => {
-                    console.error(err.message);
-                });
-            })
-            .catch(err => {
-                console.error(err.message);
-            });
-        };
-
-        document.getElementById("getButton").addEventListener("click", getFact);
-    };
+            sessionStorage.setItem("catImageURL", response.url);
+        })
+        .catch(err => {
+            console.error(err.message);
+        });
+    })
+    .catch(err => {
+        console.error(err.message);
+    });
 };
